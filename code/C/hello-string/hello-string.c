57304a00b7a349a5e979786be88cf1971cbf7354
karanaditya993 <karanaditya993@gmail.com>
2016-01-05 22:38:08 -0500

    Add hello-string exercise

---
+++ code/C/hello-string/hello-string.c
@@ -0,0 +1,16 @@
+// #include <cs50.h>
+// // cs50.h is a library that includes booleans, strings, integers that C doesn't normally have
+#include <stdio.h>
+
+int main(void){
+  // string s = "Karan";
+  // strings don't exist in C. But when you think about it, strings are actually just an array of character values. Below is how you would declare a "String" in C
+  char string[] = "Karan";
+  printf("hello, %s\n", string);
+}
+
+// However, cs50 has graciously provided us with a library that declares strings for us...so if you just add #include <cs50.h> to the top, the following code would run the same way:
+// int main(void){
+  // string s = "Karan";
+  // printf("hello, %s\n", s)
+// }
