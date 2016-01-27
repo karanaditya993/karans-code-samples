b7862ad374f0e1543020764929dd7e9cce54713f
karanaditya993 <karanaditya993@gmail.com>
2016-01-26 23:44:51 -0800

    Modify greedy to pass all testcases provided by cs50

--- code/C/greedy/greedy.c
+++ code/C/greedy/greedy.c
@@ -2,18 +2,13 @@
 #include <stdio.h>
 #include <math.h>
 
+#define quarter 25
+#define dime 10
+#define nickel 5
+
 int main(void) {
     float change;
-    int quarter = 25;
-    int dime = 10;
-    int nickel = 5;
-    int penny = 1;
-    int number_of_quarters;
-    int number_of_dimes;
-    int number_of_nickels;
-    int number_of_pennies;
-    int remaining_change;
-    int number_of_coins;
+    int number_of_quarters, number_of_dimes, number_of_nickels, remaining_change, number_of_coins, converted_change;
 
     printf("O hai! ");
     do {
@@ -22,7 +17,8 @@ int main(void) {
     }
     while (change <= 0);
 
-    int converted_change = change * 100;
+    // Must convert your change to cents
+    converted_change = (int) round(change * 100);
 
     // The round function allows for the numbers to be rounded down to their
     // nearest integer
@@ -30,18 +26,17 @@ int main(void) {
     // The modulo function gives the remainder, or the remaining amount of change
     // that must be dealt with
 
-    number_of_quarters = round(converted_change / quarter);
-    printf("Quarters: %i\n", number_of_quarters);
+    number_of_quarters = converted_change / quarter;
     remaining_change = converted_change % quarter;
-    number_of_dimes = round(remaining_change / dime);
-    printf("Dimes: %i\n", number_of_dimes);
+
+    number_of_dimes = remaining_change / dime;
     remaining_change = remaining_change % dime;
-    number_of_nickels = round(remaining_change / nickel);
-    printf("Nickels: %i\n", number_of_nickels);
+
+    number_of_nickels = remaining_change / nickel;
     remaining_change = remaining_change % nickel;
-    number_of_pennies = round(remaining_change / penny);
-    printf("Pennies: %i\n", number_of_pennies);
-    number_of_coins = number_of_quarters + number_of_dimes + number_of_nickels + number_of_pennies;
-    printf("Total Number of Coins: %i\n", number_of_coins);
+
+    number_of_coins = number_of_quarters + number_of_dimes + number_of_nickels + remaining_change;
+
+    printf("%i\n", number_of_coins);
 
 }
