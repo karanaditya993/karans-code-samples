9c4bae1cd13b438d604bc34912691e54959a31ea
karanaditya993 <karanaditya993@gmail.com>
2016-01-27 18:49:51 -0800

    Recommitting after github site maintanence

--- code/C/noswap/noswap.c
+++ code/C/noswap/noswap.c
@@ -1,3 +1,7 @@
+// This exercise shows us that even after implementing
+// a correct comparison algorithm, comparing two
+// elements does not seem to work! comparing_strings directory
+// will answer this
 #include <stdio.h>
 
 int swap(int a, int b);
