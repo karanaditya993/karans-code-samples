0232f94171018b1430e15e6772e78a6df4e82d89
karanaditya993 <karanaditya993@gmail.com>
2016-01-28 15:36:03 -0800

    Modify sizeof to include char* (String)

--- code/C/sizeof/sizeof.c
+++ code/C/sizeof/sizeof.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void) {
   char c;
   double d;
   float f;
   int i;
+  char* s;
 // On oa 32 bit system
-  printf("char: %i bytes\n", sizeof(c));
-  printf("double: %i bytes\n", sizeof(d));
-  printf("float: %i bytes\n", sizeof(f));
-  printf("int: %i bytes\n", sizeof(i));
+  printf("char: %lu bytes\n", sizeof(c));
+  printf("double: %lu bytes\n", sizeof(d));
+  printf("float: %lu bytes\n", sizeof(f));
+  printf("int: %lu bytes\n", sizeof(i));
+  printf("string: %lu bytes\n", sizeof(s));
   // %i doesn't change because we want sizeof to be represented as an integer
 }
