4c60b84e5052b4c673e6e1d74c79ef1505ea18e2
karanaditya993 <karanaditya993@gmail.com>
2016-02-01 21:45:11 -0800

    Writing to and reading from files in C

---
+++ code/C/fileIO/printer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+  // Ensure one and only command line argument
+  if (argc != 2) {
+    printf("Usage: %s [name of document]\n", argv[0]);
+    return 1;
+  }
+
+  // Open new document
+  FILE* fp = fopen(argv[1], "r");
+
+  // Error handling
+  if (fp == NULL) {
+    printf("Could not open %s\n", argv[1]);
+    return 2;
+  }
+
+  // Storage space for each line of text
+  char output[256];
+
+  // Get text from user to save to file
+  for (int i = 1; fgets(output, sizeof(output), fp) != NULL; i++) {
+    printf("Line %02d: %s", i, output);
+  }
+
+  // Close the file
+  fclose(fp);
+  return 0;
+}
