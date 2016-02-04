e82e524cbb32ddda83096886161f343e81791fb0
karanaditya993 <karanaditya993@gmail.com>
2016-02-03 18:26:37 -0800

    Revise MyPApplet file

--- code/java/examples/GUI/MyPApplet.java
+++ code/java/examples/GUI/MyPApplet.java
@@ -1,6 +1,14 @@
+// Import processing PApplet and Processing image (i.e. Processing core)
 import processing.core.*;
 
+// All PApplet GUI files will contain this file structure
 public class MyPApplet extends PApplet {
-  
+  public void setup() {
+    // setup code here
+    // This code will only work once
+  }
+  public void draw() {
+    // draw code here
+    // This code will execute in a loop
+  }
 }
-
