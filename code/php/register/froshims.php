5900b163528708795e30d88e81f31fffb570385a
karanaditya993 <karanaditya993@gmail.com>
2016-02-16 15:52:36 -0800

    Add register froshims exercise using php

---
+++ code/php/register/froshims.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+        Frosh IMs
+    </title>
+  </head>
+  <body style="text-align: center;">
+    <h1>Register for Frosh IMs</h1>
+    <form class="register" action="register.html" method="post">
+      Name: <input type="text" name="name">
+      <br/>
+      <input type="checkbox" name="captain"> Captain?
+      <br/>
+      <input type="radio" name="gender" value="M"> Male
+      <input type="radio" name="gender" value"F"> Female
+      <br/>
+      Dorm:
+      <select class="dorm" name="dorm">
+        <option value=""></option>
+        <option value="Adams">Adams</option>
+        <option value="Bradley">Bradley</option>
+        <option value="Cole">Cole</option>
+        <option value="Dejope">Dejope</option>
+        <option value="Liz Waters">Liz Waters</option>
+        <option value="Kronshage">Kronshage</option>
+        <option value="Leopold">Leopold</option>
+        <option value="Phillips">Phillips</option>
+        <option value="Slichter">Slichter</option>
+        <option value="Sullivan">Sullivan</option>
+        <option value="Barnard">Barnard</option>
+        <option value="Chadbourne">Chadbourne</option>
+        <option value="Merit House">Merit House</option>
+        <option value="Ogg">Ogg</option>
+        <option value="Sellery">Sellery</option>
+        <option value="Smith">Smith</option>
+        <option value="Susan David House">Susan David House</option>
+        <option value="Witte">Witte</option>
+      </select>
+      <br/>
+      <input type="submit" value="Register">
+    </form>
+  </body>
+</html>
