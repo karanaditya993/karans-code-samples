2ccbd463e79d66af78643e24190af0a36da9b85e
karanaditya993 <karanaditya993@gmail.com>
2016-02-16 17:09:21 -0800

    Add rerender page if false submission

--- code/php/register/register.php
+++ code/php/register/register.php
@@ -1,3 +1,11 @@
+<!--  Rerender page if one item in form is blank-->
+<?php
+  if (empty($_POST["name"]) || empty($_POST["dorm"]) || empty($_POST["gender"])) {
+    header("Location: http://ide50-karanaditya993.cs50.io/froshims.php");
+    exit;
+  }
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
