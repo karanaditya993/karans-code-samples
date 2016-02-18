40597b93dc7d6834041c0172b2172f6b206aa011
karanaditya993 <karanaditya993@gmail.com>
2016-02-17 21:11:12 -0800

    sccanf works

--- code/C/sscanf/sscanf.c
+++ code/C/sscanf/sscanf.c
@@ -10,9 +10,8 @@ int main() {
   char abs_path[100], query[100];
   char* p = "GET /hello.php?q=Alice HTTP/1.1\r\n\r\n";
   if (parse(p, abs_path, query)) {
-    printf("SUCCESS\n");
-    printf("Absolute path: %s\n", abs_path);
-    printf("Query: %s\n", query);
+    printf("Absolute Path: %s\n", abs_path);
+    printf("Query String: %s\n", query);
   }
   else {
     printf("FAILURE\n");
@@ -22,22 +21,23 @@ int main() {
 
 bool parse(const char* line, char* abs_path, char* query) {
   char method[20], request_target[100], HTTP_version[20], CRLF[20];
+  char* absolute_path, *query_string;
 
   const char question_mark = '?';
   if (line == NULL) {
     return false;
   }
   sscanf(line, "%s %s %s", method, request_target, HTTP_version);
-  abs_path = strtok(request_target, &question_mark);
-
+  absolute_path = strtok(request_target, &question_mark);
   if (request_target[0] != '/') {
     printf("Path must begin with '/' character\n");
     return false;
   }
-  query = strtok(NULL, &question_mark);
-
-  if (query == NULL) {
-    query = "";
+  query_string = strtok(NULL, &question_mark);
+  if (query_string == NULL || (strcmp(query_string, "q") == 0)) {
+    query_string = "";
   }
+  strcpy(abs_path, absolute_path);
+  strcpy(query, query_string);
   return true;
 }
