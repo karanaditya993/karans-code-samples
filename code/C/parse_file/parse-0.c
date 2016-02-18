3f467ddf23d9f4bfd5ce212b7552af790819efdb
karanaditya993 <karanaditya993@gmail.com>
2016-02-17 17:15:45 -0800

    Add parse_file, parse not working

---
+++ code/C/parse_file/parse-0.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define LimitRequestLine 8190
+
+bool parse(const char* line);
+
+int main(int argc, char* argv[]) {
+  // char abs_path[LimitRequestLine + 1];
+  // char query[LimitRequestLine + 1];
+  if (argc < 2) {
+    printf("Usage: ./parse-0 [method request_target HTTP_version CRLF]\n");
+    return 1;
+  }
+  char* request_line = argv[1];
+  if (parse(request_line)) {
+    return 0;
+  }
+  else {
+    printf("ERROR\n");
+    return 1;
+  }
+  // if (parse(line, abs_path, query)) {
+  //   printf("abs_path: %s\n", abs_path);
+  //   printf("query: %s\n", query);
+  // }
+  // else {
+  //   printf("Error in parsing the file\n");
+  //   return 1;
+  // }
+}
+
+bool parse(const char* line){
+  char method[LimitRequestLine];
+  char request_target[LimitRequestLine];
+  char HTTP_version[LimitRequestLine];
+  char CRLF[LimitRequestLine];
+
+  if (line == NULL) {
+    return false;
+  }
+  else {
+    sscanf(line, "%s %s %s %s", method, request_target, HTTP_version, CRLF);
+    printf("Method: %s\n", method);
+    printf("Request Target: %s\n", request_target);
+    printf("HTTP Version: %s\n", method);
+    printf("CRLF: %s\n", method);
+    return true;
+  }
+}
