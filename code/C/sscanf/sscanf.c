44d8e39881f3c8c3e54cfc46ecb8348428c1ccc5
karanaditya993 <karanaditya993@gmail.com>
2016-02-17 21:28:10 -0800

    Add final touches to sscanf

--- code/C/sscanf/sscanf.c
+++ code/C/sscanf/sscanf.c
@@ -8,14 +8,10 @@ char* change(char* line);
 
 int main() {
   char abs_path[100], query[100];
-  char* p = "GET /hello.php?q=Alice HTTP/1.1\r\n\r\n";
+  char* p = "GET /hello.php?q=David HTTP/1.1\r\n\r\n";
   if (parse(p, abs_path, query)) {
     printf("Absolute Path: %s\n", abs_path);
     printf("Query String: %s\n", query);
-  }
-  else {
-    printf("FAILURE\n");
-    return 1;
   }
 }
 
@@ -28,14 +24,28 @@ bool parse(const char* line, char* abs_path, char* query) {
     return false;
   }
   sscanf(line, "%s %s %s", method, request_target, HTTP_version);
+
+  // If method is not get, respond with 404 error
+  if (strcasecmp(method, "get") != 0) {
+    printf("405 Method Not Allowed\n");
+    return false;
+  }
+
   absolute_path = strtok(request_target, &question_mark);
   if (request_target[0] != '/') {
-    printf("Path must begin with '/' character\n");
+    printf("501 Not Implemented\n");
     return false;
   }
   query_string = strtok(NULL, &question_mark);
   if (query_string == NULL || (strcmp(query_string, "q") == 0)) {
     query_string = "";
+    printf("400 Bad Request\n");
+    return false;
+  }
+
+  if (strcasecmp(HTTP_version, "HTTP/1.1") != 0){
+    printf("505 HTTP Version Not Supported\n");
+    return false;
   }
   strcpy(abs_path, absolute_path);
   strcpy(query, query_string);
