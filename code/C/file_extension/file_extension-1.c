8bb6519d7dc8a6ed68c610e4cb8bb650a5cdf8e6
karanaditya993 <karanaditya993@gmail.com>
2016-02-18 11:50:34 -0800

    Reconfigure file_extension-1.c exercise

--- code/C/file_extension/file_extension-1.c
+++ code/C/file_extension/file_extension-1.c
@@ -17,18 +17,20 @@ int main(int argc, char* argv[]) {
     printf("NULL\n");
     return 1;
   }
-
   printf("%s\n", type);
 }
 
 const char* lookup(const char* path) {
   int index = -1;
-
-  const char* extensions[] = {"css", "html", "gif", "ico", "jpg", "js", "php", "png"};
+  const char* extensions[] = {".css", ".html", ".gif", ".ico", ".jpg", ".js", ".php", ".png"};
   const char* types[] = {"text/css", "text/html", "image/gif", "image/x-icon", "image/jpeg", "text/javascript", "text/x-php", "image/png"};
+  const char dot = '.';
+  char* absolute_path;
+
+  absolute_path = strchr(path, dot);
 
   for (int i = 0; i < 8; i++) {
-    if (strcasecmp(path, extensions[i]) == 0) {
+    if(strcasecmp(absolute_path, extensions[i]) == 0) {
       index = i;
     }
   }
